46d9493424ed88903ac3ff247a80f0288a6bb161
Vitor Ramos <ramos.vitor89@gmail.com>
2016-05-08 14:45:35 -0300

    login adicionado

--- supermercado/forms.py
+++ supermercado/forms.py
@@ -2,7 +2,12 @@ from django import forms
 from models import *
 
 
-class ClienteForm(forms.Form):
+class CadastroForm(forms.Form):
         nome = forms.CharField(max_length=45)
         senha = forms.CharField(max_length=45, widget=forms.PasswordInput())
         CPF = forms.CharField(max_length=45)
+
+
+class LoginForm(forms.Form):
+        nome= forms.CharField(max_length=45)
+        senha= forms.CharField(max_length=45, widget=forms.PasswordInput())
--- supermercado/views.py
+++ supermercado/views.py
@@ -6,14 +6,28 @@ def index(request):
     return render(request, 'home.html')
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "POST" and 'voltar' in request.POST:
+        return render(request, 'home.html')
+
+    elif request.method == "POST" and 'submit' in request.POST:
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            user = Usuario()
+            user.nome = form.cleaned_data.get('nome')
+            user.senha = form.cleaned_data.get('senha')
+
+    else:
+        form = LoginForm()
+
+    return render(request, 'login.html', {'form': form})
 
 def cadastro(request):
     if request.method == "POST" and 'voltar' in request.POST:
         return render(request, 'home.html')
 
     elif request.method == "POST" and 'submit' in request.POST:
-        form = ClienteForm(request.POST)
+        form = CadastroForm(request.POST)
 
         if form.is_valid():
             user = Usuario()
@@ -25,7 +39,7 @@ def cadastro(request):
             cliente.CPF = form.cleaned_data.get('CPF')
             cliente.save()
     else:
-        form = ClienteForm()
+        form = CadastroForm()
     return render(request, 'cadastro.html', {'form': form, 'clientes': Cliente.objects.all()})
 
 def sobre(request):
