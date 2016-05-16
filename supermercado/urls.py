55a8387449c1df169c01bf5b80456f48cecaa53b
Vitor Ramos <ramos.vitor89@gmail.com>
2016-05-16 20:12:37 -0300

    link url

--- supermercado/urls.py
+++ supermercado/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     url(r'^favoritos/$', favoritos, name='favoritos'),
     url(r'^produtos/$', produtos, name='produtos'),
     url(r'^cadastroDono/', cadastroDono, name='dono'),
+    url(r'^pesquisa/', pesquisa, name='pesquisa'),
     url(r'^$', index, name='index'),
 ]
